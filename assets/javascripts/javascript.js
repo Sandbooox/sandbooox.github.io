a06f420bc54367055bddebe562812fe6d3218b8e
Sandbooox <iohancamaleon777@gmail.com>
2024-05-25 21:56:54 -0300

    Create javascript.js

---
+++ assets/javascripts/javascript.js
@@ -0,0 +1,4 @@
+console.log("%cSandbooox.github.io", "font-size: 20px; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;");
+console.log("-------------------------------")
+console.log("Welcome to the page! 🎉")
+console.log("My Page https://Sandbooox.github.io")
