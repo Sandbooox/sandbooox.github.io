e60c221a2b18eae689930d0111412c4fb1d744f7
Sandbooox <sandboox235@gmail.com>
2024-06-24 02:50:50 -0300

    Add files via upload

---
+++ clock/assets/javascript/javascript.js
@@ -0,0 +1,17 @@
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const dayElement = document.getElementById('day');
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const day = daysOfWeek[now.getDay()];
+
+    dayElement.textContent = day;
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime(); // initial call to display time immediately
