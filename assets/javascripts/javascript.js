6805febe3d8683e4dde36d304c10d93c93821046
Sandbooox <sandboox235@gmail.com>
2024-07-01 06:55:38 -0300

    Delete assets directory

--- assets/javascripts/javascript.js
+++
@@ -1,4 +0,0 @@
-console.log("%cSandbooox.github.io", "font-size: 20px; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-weight: bold;");
-console.log("-------------------------------")
-console.log("Welcome to the page! 🎉")
-console.log("My Page https://Sandbooox.github.io")
